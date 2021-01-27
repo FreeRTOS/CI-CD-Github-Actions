a38512fd3f66549a128961e0320ab63937148b9f
Archit Gupta <archigup@amazon.com>
2021-01-27 14:27:53 -0800

    Clean up comments

--- memory_statistics/test/test.c
+++ memory_statistics/test/test.c
@@ -1,11 +1,14 @@
 #include "stdint.h"
-// Test added include directories
+
+// Test include directories added from memory_statistics config.
+// This header is in ./include which is in the test config's includes.
 #include "test_header.h"
 
-// Test added compiler flags
+// Test compiler flags added from memory_statististics config.
+// TEST_FLAG is set in the test config's compiler_flags array.
 #ifdef TEST_FLAG
 
-// This should add 10340 bytes to .text
-// The results should be memory_statistics reporting 10.1K for this file
+// This should add 10340 bytes to the .text section.
+// The result should be memory_statistics reporting 10.1K for this file.
 const uint8_t array[10340] = {1};
 #endif
