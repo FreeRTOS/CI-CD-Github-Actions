dee4776669c254c1049344f28fb3c3b218073506
Archit Gupta <archigup@amazon.com>
2021-01-27 14:18:41 -0800

    Include feedback

---
+++ memory_statistics/test/include/test_header.h
@@ -0,0 +1,5 @@
+/**
+ * test_header.h
+ * This is an empty header file to test the include directories configuration of
+ * the memory_statistics action.
+ */
--- memory_statistics/test/test.c
+++ memory_statistics/test/test.c
@@ -1,4 +1,11 @@
+#include "stdint.h"
+// Test added include directories
 #include "test_header.h"
+
+// Test added compiler flags
 #ifdef TEST_FLAG
-const char array[10340] = {1};
+
+// This should add 10340 bytes to .text
+// The results should be memory_statistics reporting 10.1K for this file
+const uint8_t array[10340] = {1};
 #endif
