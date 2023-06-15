7816f55ec7ac4cb4bcaf7e1bd79eb740e8e98c4f
Xiaodong Li <xiaodonn@amazon.com>
2023-06-15 10:40:55 -0700

    get git ip address

--- git-ip-address/git_ip_address.py
+++ git-ip-address/git_ip_address.py
@@ -3,10 +3,10 @@ import socket
 
 def get_ip_address():
     git_hostname =  socket.gethostname()
-    # ip_address = socket.gethostbyname(git_hostname)
+    ip_address = socket.gethostbyname(git_hostname)
     # open("get_ip_address.txt", "w").write(ip_address)
     print(git_hostname)
-    # print(ip_address)
+    print(ip_address)
     return git_hostname
 
 if __name__ == "__main__":
