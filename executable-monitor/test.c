bfb038f6a895c89a6633bd54ee2a45c134540294
Soren Ptak <ptaksoren@gmail.com>
2023-07-28 18:57:44 -0700

    Make it so the test compiles for windows so I can add windows tests to the workflow

--- executable-monitor/test.c
+++ executable-monitor/test.c
@@ -1,8 +1,36 @@
-#include <sys/time.h>
+
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdint.h>
+
+#if defined(WIN32) || defined(_WIN32) || defined (__WIN32__) || defined(__NT__) || defined(WIN64) || defined(__WIN64)
+    #include <Windows.h>
+    // Remove the warning about implicit sleep even with windows included
+    extern void sleep(int miliseconds);
+int gettimeofday(struct timeval * tp, void * superImportantVariable)
+{
+    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
+    // until 00:00:00 January 1, 1970
+    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+    SYSTEMTIME  system_time;
+    FILETIME    file_time;
+    uint64_t    time;
+
+    GetSystemTime( &system_time );
+    SystemTimeToFileTime( &system_time, &file_time );
+    time =  ((uint64_t)file_time.dwLowDateTime )      ;
+    time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+    return 0;
+}
+
+#else
+    #include <sys/time.h>
+    #include <unistd.h>
+#endif
 
 
 
